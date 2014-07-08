43c4caccf3bf31caa11cb3bb2f7609593465cd2e
runicalp <runicalp@gmail.com>
2014-07-08 19:13:45 +0900

    implement time control

--- LifeGame/MainWindow.xaml.cs
+++ LifeGame/MainWindow.xaml.cs
@@ -14,8 +14,7 @@ namespace LifeGame
         public MainWindow()
         {
             InitializeComponent();
-            var viewModel = (ViewModel)DataContext;
-            var wb = new WriteableBitmap(viewModel.CellBoard.Width, viewModel.CellBoard.Height, 96, 96, PixelFormats.Bgr32, null);
+            var wb = new WriteableBitmap(ViewModel.CellBoard.Width, ViewModel.CellBoard.Height, 96, 96, PixelFormats.Bgr32, null);
             canvas.Source = wb;
             // RenderOptions.SetBitmapScalingMode(canvas, BitmapScalingMode.NearestNeighbor);
 
@@ -24,26 +23,29 @@ namespace LifeGame
                 wb.Lock();
                 unsafe
                 {
-                    CellBoard.Render((uint*)wb.BackBuffer, wb.BackBufferStride, viewModel.CellBoard.Width, viewModel.CellBoard.Height, viewModel.CellBoard.Cells);
+                    CellBoard.Render((uint*)wb.BackBuffer, wb.BackBufferStride, ViewModel.CellBoard.Width, ViewModel.CellBoard.Height, ViewModel.CellBoard.Cells);
                 }
                 wb.AddDirtyRect(new Int32Rect(0, 0, wb.PixelWidth, wb.PixelHeight));
                 wb.Unlock();
             };
+            //PlayPauseButton.IsChecked = ViewModel.IsPlay;
         }
+
+        ViewModel ViewModel { get { return (ViewModel)DataContext; } }
 
         private void PlayCommand_OnExecuted(object sender, ExecutedRoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            ViewModel.Play();
         }
 
         private void PauseCommand_OnExecuted(object sender, ExecutedRoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            ViewModel.Pause();
         }
 
         private void StopCommand_OnExecuted(object sender, ExecutedRoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            ViewModel.Stop();
         }
     }
 }
--- LifeGame/ViewModel.cs
+++ LifeGame/ViewModel.cs
@@ -1,14 +1,28 @@
 using System;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows.Threading;
+using LifeGame.Annotations;
 
 namespace LifeGame
 {
-    class ViewModel
+    class ViewModel : INotifyPropertyChanged
     {
+        private readonly DispatcherTimer dispatcherTimer;
+        private bool isPlay;
         public CellBoard CellBoard { get; private set; }
 
         public ViewModel()
+        {
+            Initialize();
+
+            dispatcherTimer = new DispatcherTimer(DispatcherPriority.Normal) { Interval = new TimeSpan(0, 0, 0, 0, 10) };
+            dispatcherTimer.Tick += (sender1, e) => Update();
+            Play();
+        }
+
+        private void Initialize()
         {
             CellBoard = new CellBoard(640, 640);
             const string patern = @"
@@ -36,22 +50,56 @@ namespace LifeGame
                                 ¡¡¡           
                                 ¡             
                                  ¡            ";
-            
-            foreach(var c in patern
+
+            foreach (var c in patern
                 .Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
-                .SelectMany((s, y) => s.Select((c, x) => new {x, y, visible = c == '¡'})))
+                .SelectMany((s, y) => s.Select((c, x) => new { x, y, visible = c == '¡' })))
             {
                 CellBoard[c.x, c.y] = c.visible;
             }
-
-            var dispatcherTimer = new DispatcherTimer(DispatcherPriority.Normal) { Interval = new TimeSpan(0, 0, 0, 0, 10) };
-            dispatcherTimer.Tick += (sender1, e) => Update();
-            dispatcherTimer.Start();
         }
 
         private void Update()
         {
             CellBoard.NextGeneration();
+        }
+
+        public bool IsPlay
+        {
+            get { return isPlay; }
+            private set
+            {
+                if (value.Equals(isPlay)) return;
+                isPlay = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void Play()
+        {
+            dispatcherTimer.Start();
+            IsPlay = true;
+        }
+
+        public void Pause()
+        {
+            dispatcherTimer.Stop();
+            IsPlay = false;
+        }
+
+        public void Stop()
+        {
+            Pause();
+            Initialize();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
