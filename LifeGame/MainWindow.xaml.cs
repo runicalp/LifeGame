d1d86eb1a797d9b80283c94f5107b9e9ee6941e9
runicalp <runicalp@gmail.com>
2014-07-10 12:43:27 +0900

    Implemented canvas move

--- LifeGame/MainWindow.xaml.cs
+++ LifeGame/MainWindow.xaml.cs
@@ -50,21 +50,21 @@ namespace LifeGame
         }
     }
 
-    public class MyMouseDragElementBehavior : Behavior<FrameworkElement>
+    public class MouseScrollElementBehavior : Behavior<FrameworkElement>
     {
-        private bool _IsDrag = false;
-        private Point _MouseStartPosition;
-        private Point _ElementStartPosition;
+        private bool isDrag;
+        private Point mouseStartPosition;
+        private Point elementStartPosition;
 
-        #region Canvas
-        public Canvas Canvas
+        #region ScrollViewer
+        public ScrollViewer ScrollViewer
         {
-            get { return (Canvas)GetValue(CanvasProperty); }
-            set { SetValue(CanvasProperty, value); }
+            get { return (ScrollViewer)GetValue(ScrollViewerProperty); }
+            set { SetValue(ScrollViewerProperty, value); }
         }
 
-        public static readonly DependencyProperty CanvasProperty =
-            DependencyProperty.Register("Canvas", typeof(Canvas), typeof(MyMouseDragElementBehavior), new PropertyMetadata(null));
+        public static readonly DependencyProperty ScrollViewerProperty =
+            DependencyProperty.Register("ScrollViewer", typeof(ScrollViewer), typeof(MouseScrollElementBehavior), new PropertyMetadata(null));
         #endregion
         
         #region LockX
@@ -75,7 +75,7 @@ namespace LifeGame
         }
 
         public static readonly DependencyProperty LockXProperty =
-            DependencyProperty.Register("LockX", typeof(bool), typeof(MyMouseDragElementBehavior), new PropertyMetadata(false));
+            DependencyProperty.Register("LockX", typeof(bool), typeof(MouseScrollElementBehavior), new PropertyMetadata(false));
         #endregion
 
         #region LockY
@@ -86,87 +86,57 @@ namespace LifeGame
         }
 
         public static readonly DependencyProperty LockYProperty =
-            DependencyProperty.Register("LockY", typeof(bool), typeof(MyMouseDragElementBehavior), new PropertyMetadata(false));
+            DependencyProperty.Register("LockY", typeof(bool), typeof(MouseScrollElementBehavior), new PropertyMetadata(false));
         #endregion
 
         protected override void OnAttached()
         {
             base.OnAttached();
-            AssociatedObject.MouseLeftButtonDown += AssociatedObject_MouseLeftButtonDown;
-            AssociatedObject.MouseMove += AssociatedObject_MouseMove;
-            AssociatedObject.MouseLeftButtonUp += AssociatedObject_MouseLeftButtonUp;
+            AssociatedObject.MouseLeftButtonDown += MouseLeftButtonDown;
+            AssociatedObject.MouseMove += MouseMove;
+            AssociatedObject.MouseLeftButtonUp += MouseLeftButtonUp;
         }
 
         protected override void OnDetaching()
         {
-            AssociatedObject.MouseLeftButtonDown -= AssociatedObject_MouseLeftButtonDown;
-            AssociatedObject.MouseMove -= AssociatedObject_MouseMove;
-            AssociatedObject.MouseRightButtonUp -= AssociatedObject_MouseLeftButtonUp;
+            AssociatedObject.MouseLeftButtonDown -= MouseLeftButtonDown;
+            AssociatedObject.MouseMove -= MouseMove;
+            AssociatedObject.MouseRightButtonUp -= MouseLeftButtonUp;
             base.OnDetaching();
         }
 
-        void AssociatedObject_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        void MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (Canvas == null) return;
+            if (ScrollViewer == null) return;
 
-            _IsDrag = true;
-            // ドラッグ開始時のマウスと要素の位置を取得する。
-            _MouseStartPosition = e.GetPosition(Canvas);
-
-            _ElementStartPosition = new Point(
-                Canvas.GetLeft(AssociatedObject),
-                Canvas.GetTop(AssociatedObject));
-            if (double.IsNaN(_ElementStartPosition.X)) _ElementStartPosition.X = 0.0;
-            if (double.IsNaN(_ElementStartPosition.Y)) _ElementStartPosition.Y = 0.0;
-
+            isDrag = true;
             AssociatedObject.CaptureMouse();
+
+            mouseStartPosition = e.GetPosition(ScrollViewer);
+
+            elementStartPosition = new Point(
+                ScrollViewer.HorizontalOffset,
+                ScrollViewer.VerticalOffset);
+
         }
 
-        void AssociatedObject_MouseMove(object sender, MouseEventArgs e)
+        void MouseMove(object sender, MouseEventArgs e)
         {
-            if (Canvas == null) return;
+            if (ScrollViewer == null) return;
+            if (!isDrag) return;
 
-            if (_IsDrag)
-            {
-                // 移動後の要素の位置を計算する
-                var currentMousePos = e.GetPosition(Canvas);
-                var currentElementPos = _ElementStartPosition
-                                                + (currentMousePos - _MouseStartPosition);
+            var mouseCurrentPosition = e.GetPosition(ScrollViewer);
+            var scrollOffset = elementStartPosition - (mouseCurrentPosition - mouseStartPosition);
 
-                // Canvas内に収まる様に補正する
-//                var maxX = Canvas.ActualWidth - AssociatedObject.ActualWidth;
-//                if (currentElementPos.X < 0)
-//                {
-//                    currentElementPos.X = 0.0;
-//                }
-//                else if (maxX < currentElementPos.X)
-//                {
-//                    currentElementPos.X = maxX;
-//                }
-//
-//                var maxY = Canvas.ActualHeight - AssociatedObject.ActualHeight;
-//                if (currentElementPos.Y < 0)
-//                {
-//                    currentElementPos.Y = 0.0;
-//                }
-//                else if (maxY < currentElementPos.Y)
-//                {
-//                    currentElementPos.Y = maxY;
-//                }
-
-                // 要素を移動させる
-                if (!LockX) Canvas.SetLeft(AssociatedObject, currentElementPos.X);
-                if (!LockY) Canvas.SetTop(AssociatedObject, currentElementPos.Y);
-            }
+            if (!LockX) ScrollViewer.ScrollToHorizontalOffset(scrollOffset.X);
+            if (!LockY) ScrollViewer.ScrollToVerticalOffset(scrollOffset.Y);
         }
 
-        void AssociatedObject_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        void MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            if (_IsDrag)
-            {
-                _IsDrag = false;
+            isDrag = false;
+            if (AssociatedObject.IsMouseCaptured)
                 AssociatedObject.ReleaseMouseCapture();
-            }
         }
     }
 }
