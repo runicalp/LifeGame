c4b17ca4140f6b639612d033f1a49512c07b9fd1
runicalp <runicalp@gmail.com>
2014-07-14 14:57:14 +0900

    Merge develop

--- LifeGame/MainWindow.xaml.cs
+++ LifeGame/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
@@ -12,21 +14,62 @@ namespace LifeGame
         public MainWindow()
         {
             InitializeComponent();
-            var viewModel = (ViewModel)DataContext;
-            var wb = new WriteableBitmap(viewModel.CellBoard.Width, viewModel.CellBoard.Height, 96, 96, PixelFormats.Bgr32, null);
-            canvas.Source = wb;
-            // RenderOptions.SetBitmapScalingMode(canvas, BitmapScalingMode.NearestNeighbor);
+            AntiAlias = false;
+            var wb = new WriteableBitmap(ViewModel.CellBoard.Width, ViewModel.CellBoard.Height, 96, 96, PixelFormats.Bgr32, null);
+            Canvas.Source = wb;
 
             CompositionTarget.Rendering += (sender, args) =>
             {
                 wb.Lock();
                 unsafe
                 {
-                    CellBoard.Render((uint*)wb.BackBuffer, wb.BackBufferStride, viewModel.CellBoard.Width, viewModel.CellBoard.Height, viewModel.CellBoard.Cells);
+                    CellBoard.Render((uint*)wb.BackBuffer, wb.BackBufferStride, ViewModel.CellBoard.Width, ViewModel.CellBoard.Height, ViewModel.CellBoard.Cells);
                 }
                 wb.AddDirtyRect(new Int32Rect(0, 0, wb.PixelWidth, wb.PixelHeight));
                 wb.Unlock();
             };
+        }
+
+        ViewModel ViewModel { get { return (ViewModel)DataContext; } }
+
+        private void PlayCommand_OnExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            ViewModel.Play();
+        }
+
+        private void PauseCommand_OnExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            ViewModel.Pause();
+        }
+
+        private void StopCommand_OnExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            ViewModel.Stop();
+        }
+
+        public bool AntiAlias
+        {
+            set
+            {
+                RenderOptions.SetBitmapScalingMode(Canvas,
+                    value ? BitmapScalingMode.Linear : BitmapScalingMode.NearestNeighbor);
+            }
+            get { return RenderOptions.GetBitmapScalingMode(Canvas) != BitmapScalingMode.NearestNeighbor; }
+        }
+
+        private void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 1)
+            {
+                var p = e.GetPosition(Canvas);
+                ViewModel.CellBoard[(int)Math.Floor(p.X), (int)Math.Floor(p.Y)] = !ViewModel.CellBoard[(int)Math.Floor(p.X), (int)Math.Floor(p.Y)];
+                ViewModel.CellBoard.CalcuratePopulation();
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Play();
         }
     }
 }
--- LifeGame/ViewModel.cs
+++ LifeGame/ViewModel.cs
@@ -1,16 +1,36 @@
 using System;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows.Threading;
+using LifeGame.Annotations;
 
 namespace LifeGame
 {
-    class ViewModel
+    sealed class ViewModel : INotifyPropertyChanged
     {
+        private readonly DispatcherTimer dispatcherTimer;
+        private bool isPlay;
         public CellBoard CellBoard { get; private set; }
 
         public ViewModel()
         {
+            Initialize();
+
+            dispatcherTimer = new DispatcherTimer(DispatcherPriority.Normal) { Interval = new TimeSpan(0, 0, 0, 0, 10) };
+            dispatcherTimer.Tick += (sender1, e) => Update();
+        }
+
+        private void Initialize()
+        {
+            if (CellBoard != null)
+            {
+                CellBoard.PropertyChanged -= CellBoardOnPropertyChanged;
+            }
+
             CellBoard = new CellBoard(640, 640);
+            CellBoard.PropertyChanged += CellBoardOnPropertyChanged;
+
             const string patern = @"
                                               
                                               
@@ -36,22 +56,71 @@ namespace LifeGame
                                 ¡¡¡           
                                 ¡             
                                  ¡            ";
-            
-            foreach(var c in patern
+
+            foreach (var c in patern
                 .Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
-                .SelectMany((s, y) => s.Select((c, x) => new {x, y, visible = c == '¡'})))
+                .SelectMany((s, y) => s.Select((c, x) => new { x, y, visible = c == '¡' })))
             {
                 CellBoard[c.x, c.y] = c.visible;
             }
 
-            var dispatcherTimer = new DispatcherTimer(DispatcherPriority.Normal) { Interval = new TimeSpan(0, 0, 0, 0, 10) };
-            dispatcherTimer.Tick += (sender1, e) => Update();
-            dispatcherTimer.Start();
+            CellBoard.Step = 0;
+            CellBoard.CalcuratePopulation();
+        }
+
+        private void CellBoardOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            if (propertyChangedEventArgs.PropertyName == "Step")
+                OnPropertyChanged("Step");
+            if (propertyChangedEventArgs.PropertyName == "Population")
+                OnPropertyChanged("Population");
         }
 
         private void Update()
         {
             CellBoard.NextGeneration();
+        }
+
+        public bool IsPlay
+        {
+            get { return isPlay; }
+            private set
+            {
+                if (value.Equals(isPlay)) return;
+                isPlay = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void Play()
+        {
+            dispatcherTimer.Start();
+            IsPlay = true;
+        }
+
+        public void Pause()
+        {
+            dispatcherTimer.Stop();
+            IsPlay = false;
+        }
+
+        public void Stop()
+        {
+            Pause();
+            Initialize();
+        }
+
+        public int Step { get { return CellBoard.Step; } }
+
+        public int Population { get { return CellBoard.Population; } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
